031e82fc55ab0fedbe9418ed97500f5e37c2ace8
okonayakodji <okonayakodji@mail.ru>
2023-09-07 17:43:13 +0300

    Basic transliteration

--- src/good_names_generator.py
+++ src/good_names_generator.py
@@ -4,6 +4,7 @@ parser = ArgumentParser(prog="good_names_generator")
 parser.add_argument("--from", default="list.txt", dest="source_file", type=str, help="Path to txt file with input names")
 parser.add_argument("--to", default="list.txt", type=str, help="Path to file with generated results")
 parser.add_argument("-og", "--off-generator", action="store_true", help="Off generating files")
+parser.add_argument("-t", "--transliteration", action="store_true", help="Enable transliteration")
 args = parser.parse_args()
 
 
@@ -37,14 +38,15 @@ def transliterate(name):
 
 
 def main():
-    print(transliterate("Пономарев Дмитрий Сергеевич"))
-    return
     result = []
     
     with open(args.source_file, "r") as source_file:
         for n, line in enumerate(source_file):
             try:
-                result.append(from_bad_name_to_good(line))
+                new_line = from_bad_name_to_good(line)
+                if args.transliteration:
+                    new_line = transliterate(new_line).lower()
+                result.append(new_line)
             except IndexError:
                 print(f"Something wrong with {n+1} line ({repr(line)})")
     
