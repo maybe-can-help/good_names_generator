af286e127037052050f3fd3bc332f126c37eeee4
okonayakodji <okonayakodji@mail.ru>
2023-09-08 14:17:36 +0300

    New version

--- src/good_names_generator.py
+++ src/good_names_generator.py
@@ -1,42 +1,12 @@
 from argparse import ArgumentParser
-from sys import stdin, argv
-
-parser = ArgumentParser(prog="good_names_generator")
-parser.add_argument("--from",
-                    dest="input",
-                    default="list.txt",
-                    type=str,
-                    help="Path to txt file with input names")
-parser.add_argument("--to",
-                    dest="output",
-                    default="list.txt",
-                    type=str,
-                    help="Path to file with generated results")
-parser.add_argument("-og",
-                    "--off-generator",
-                    action="store_true",
-                    help="Off generating files")
-parser.add_argument("-t",
-                    "--transliteration",
-                    action="store_true",
-                    help="Enable transliteration")
-args = parser.parse_args()
+from sys import stdin
 
 
-def append_telesales_mail_address(string: str,
-                                  postfix: str = "@telesales-service.org") -> str:
-    return f"{string}{postfix}"
+def add_email_prefix(line: str, postfix: str = "@telesales-service.org") -> str:
+    return f"{line}{postfix}"
 
 
-def from_bad_name_to_good(name: str) -> str:
-    separated_name = name.split()
-    surname = separated_name[0]
-    name = separated_name[1]
-    patronymic = separated_name[2]
-    return f"{surname}.{name[0]}.{patronymic[0]}"
-
-    
-def transliterate(name):
+def transliterate(string):
     _dict = {'а':'a','б':'b','в':'v','г':'g','д':'d','е':'e','ё':'e',
           'ж':'zh','з':'z','и':'i','й':'i','к':'k','л':'l','м':'m','н':'n',
           'о':'o','п':'p','р':'r','с':'s','т':'t','у':'u','ф':'f','х':'h',
@@ -51,46 +21,119 @@ def transliterate(name):
           '[':'',']':'','{':'','}':'','ґ':'','ї':'', 'є':'','Ґ':'g','Ї':'i',
           'Є':'e', '—':''}
     for key in _dict:
-        name = name.replace(key, _dict[key])
-    return name
+        string = string.replace(key, _dict[key])
+    return string 
 
 
-def main():
-    result = []
-    # If true - terminal, false - pipe mode
-    if stdin.isatty():
-        with open(args.input, "r", encoding="utf-8") as source_file:
-            for n, line in enumerate(source_file):
-                try:
-                    if args.transliteration:
-                        new_line = from_bad_name_to_good(transliterate(line).lower()) + "\n"
-                    else:
-                        new_line = from_bad_name_to_good(line) + "\n"
-                    result.append(new_line)
-                except IndexError:
-                    print(f"Something wrong with {n+1} line ({repr(line)})")
-        if not args.off_generator:
-            with open(args.output, "w", encoding="utf-8") as out_file:
-                for line in result:
-                    out_file.write(line)
+def generate_cert_name(name: [str], transliteration: bool = True) -> str:
+    if transliteration:
+        name = transliterate(name)
+    separated_name = name.split() 
+    separated_name_length = len(separated_name)
+    surname = separated_name[0]
+    name = separated_name[1]
+    if separated_name_length >= 3: 
+        patronymic = separated_name[2]
+        return f"{surname}.{name[0]}.{patronymic[0]}".lower()
     else:
-        for n, line in enumerate(stdin):
-            try:
-                if args.transliteration:
-                    new_line = from_bad_name_to_good(transliterate(line).lower())
-                else:
-                    new_line = from_bad_name_to_good(line)
-                result.append(new_line)
-            except IndexError:
-                print(f"Something wrong with {n+1} line ({repr(line)})")
-        if not args.off_generator:
-            with open(args.output, "w", encoding="utf-8") as out_file:
-                for line in result:
-                    out_file.write(line)
-        else:
-            for line in result:
-                print(repr(line))
+        return f"{surname}.{name[0]}".lower()
 
-    
-if __name__ == "__main__":
-    main()
+
+def emails(argparse_arguments):
+    result = []
+    if stdin.isatty():
+        try:
+            inputfile = open(argparse_arguments.inputfile, "r", encoding=argparse_arguments.encoding)
+        except FileNotFoundError:
+            print(f"File {argparse_arguments.inputfile} does not exists")
+        try:
+            for n, line in enumerate(inputfile):
+                result.append(add_email_prefix(generate_cert_name(line)) + "\n")
+        except IndexError:
+            print(f"Something wrong with {n+1} line ({repr(line)})")
+        inputfile.close()
+    if not stdin.isatty():
+        for line in stdin:
+            result.append(add_email_prefix(generate_cert_name(line)) + "\n")
+    result = set(result)
+
+    if not argparse_arguments.disable_generator:
+        try:
+            if argparse_arguments.append_to_outputfile:
+                outputfile = open(argparse_arguments.outputfile, "a", encoding=argparse_arguments.encoding)
+            else:
+                outputfile = open(argparse_arguments.outputfile, "w", encoding=argparse_arguments.encoding)
+        except Exception as e:
+            raise e
+        for line in result:
+            outputfile.write(line)
+        outputfile.close()
+    if argparse_arguments.enable_printing:
+        for line in result:
+            print(repr(line))
+
+
+def certnames(argparse_arguments):
+    result = []
+    if stdin.isatty():
+        try:
+            inputfile = open(argparse_arguments.inputfile, "r", encoding=argparse_arguments.encoding)
+        except FileNotFoundError:
+            print(f"File {argparse_arguments.inputfile} does not exists")
+        try:
+            for n, line in enumerate(inputfile):
+                result.append(generate_cert_name(line) + "\n")
+        except IndexError:
+            print(f"Something wrong with {n+1} line ({repr(line)})")
+        inputfile.close()
+    if not stdin.isatty():
+        for line in stdin:
+            result.append(generate_cert_name(line) + "\n")
+    result = set(result)
+
+    if not argparse_arguments.disable_generator:
+        try:
+            if argparse_arguments.append_to_outputfile:
+                outputfile = open(argparse_arguments.outputfile, "a", encoding=argparse_arguments.encoding)
+            else:
+                outputfile = open(argparse_arguments.outputfile, "w", encoding=argparse_arguments.encoding)
+        except Exception as e:
+            raise e
+        for line in result:
+            outputfile.write(line)
+        outputfile.close()
+    if argparse_arguments.enable_printing:
+        for line in result:
+            print(repr(line))
+
+
+parser = ArgumentParser("Good names generator")
+
+
+commands = parser.add_subparsers()
+generator = commands.add_parser("generate")
+generator_commands = generator.add_subparsers()
+
+
+parser_certnames = generator_commands.add_parser("certnames")
+parser_certnames.add_argument("--from", dest="inputfile", help="Path to file with names")
+parser_certnames.add_argument("--to", dest="outputfile", help="Path to output file (Will be generated if does not exist)")
+parser_certnames.add_argument("-dg", "--disable-generator", action="store_true", help="Path to output file (Will be generated if does not exist)")
+parser_certnames.add_argument("-a", "--append", dest="append_to_outputfile", action="store_true", help="Path to output file (Will be generated if does not exist)")
+parser_certnames.add_argument("-p", "--printing", dest="enable_printing", action="store_true", help="Path to output file (Will be generated if does not exist)")
+parser_certnames.add_argument("--encoding", type=str, default="utf-8", help="Path to output file (Will be generated if does not exist)")
+parser_certnames.set_defaults(func=certnames)
+
+
+parser_emails = generator_commands.add_parser("emails")
+parser_emails.add_argument("--from", dest="inputfile", help="Path to file with names")
+parser_emails.add_argument("--to", dest="outputfile", help="Path to output file (Will be generated if does not exist)")
+parser_emails.add_argument("-dg", "--disable-generator", action="store_true", help="Path to output file (Will be generated if does not exist)")
+parser_emails.add_argument("-a", "--append", dest="append_to_outputfile", action="store_true", help="Path to output file (Will be generated if does not exist)")
+parser_emails.add_argument("-p", "--printing", dest="enable_printing", action="store_true", help="Path to output file (Will be generated if does not exist)")
+parser_emails.add_argument("--encoding", type=str, default="utf-8", help="Path to output file (Will be generated if does not exist)")
+parser_emails.set_defaults(func=emails)
+
+
+args = parser.parse_args()
+args.func(args)
