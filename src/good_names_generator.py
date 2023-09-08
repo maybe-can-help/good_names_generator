2131c14709a66937f99e95eee98c379905943bb0
okonayakodji <okonayakodji@mail.ru>
2023-09-08 09:49:26 +0300

    feat(pipe_mode): Add check for the pipe mode

--- src/good_names_generator.py
+++ src/good_names_generator.py
@@ -1,20 +1,38 @@
 from argparse import ArgumentParser
+from sys import stdin, argv
 
 parser = ArgumentParser(prog="good_names_generator")
-parser.add_argument("--from", default="list.txt", dest="source_file", type=str, help="Path to txt file with input names")
-parser.add_argument("--to", default="list.txt", type=str, help="Path to file with generated results")
-parser.add_argument("-og", "--off-generator", action="store_true", help="Off generating files")
-parser.add_argument("-t", "--transliteration", action="store_true", help="Enable transliteration")
+parser.add_argument("-i",
+                    "--input",
+                    default="list.txt",
+                    type=str,
+                    help="Path to txt file with input names")
+parser.add_argument("-o",
+                    "--output",
+                    default="list.txt",
+                    type=str,
+                    help="Path to file with generated results")
+parser.add_argument("-og",
+                    "--off-generator",
+                    action="store_true",
+                    help="Off generating files")
+parser.add_argument("-t",
+                    "--transliteration",
+                    action="store_true",
+                    help="Enable transliteration")
 args = parser.parse_args()
 
 
-def append_telesales_mail_address(string: str, postfix: str = "@telesales-service.org") -> str:
+def append_telesales_mail_address(string: str,
+                                  postfix: str = "@telesales-service.org") -> str:
     return f"{string}{postfix}"
 
 
 def from_bad_name_to_good(name: str) -> str:
     separated_name = name.split()
-    surname, name, patronymic = separated_name[0], separated_name[1], separated_name[2]
+    surname = separated_name[0]
+    name = separated_name[1]
+    patronymic = separated_name[2]
     return f"{surname}.{name[0]}.{patronymic[0]}\n"
 
     
@@ -40,21 +58,27 @@ def transliterate(name):
 def main():
     result = []
     
-    with open(args.source_file, "r") as source_file:
-        for n, line in enumerate(source_file):
-            try:
-                new_line = from_bad_name_to_good(line)
-                if args.transliteration:
-                    new_line = transliterate(new_line).lower()
-                result.append(new_line)
-            except IndexError:
-                print(f"Something wrong with {n+1} line ({repr(line)})")
+    # If true - Terminal, false - pipe mode
+    if stdin.isatty():
+        with open(args.input, "r") as source_file:
+            for n, line in enumerate(source_file):
+                try:
+                    new_line = from_bad_name_to_good(line)
+                    if args.transliteration:
+                        new_line = transliterate(new_line).lower()
+                    result.append(new_line)
+                except IndexError:
+                    print(f"Something wrong with {n+1} line ({repr(line)})")
+        
+        
+        if not args.off_generator:
+            with open(args.output, "w") as out_file:
+                for line in result:
+                    out_file.write(line)
+    else:
+        print("Pipe")
+
     
-    
-    if not args.off_generator:
-        with open(args.to, "w") as out_file:
-            for line in result:
-                out_file.write(line)
 
 
 if __name__ == "__main__":
