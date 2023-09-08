7334add66357215b73a1d5c3f98bf4c70d798216
okonayakodji <okonayakodji@mail.ru>
2023-09-08 12:13:04 +0300

    fix: Change printing in saving to file

--- src/good_names_generator.py
+++ src/good_names_generator.py
@@ -2,13 +2,13 @@ from argparse import ArgumentParser
 from sys import stdin, argv
 
 parser = ArgumentParser(prog="good_names_generator")
-parser.add_argument("-i",
-                    "--input",
+parser.add_argument("--from",
+                    dest="input",
                     default="list.txt",
                     type=str,
                     help="Path to txt file with input names")
-parser.add_argument("-o",
-                    "--output",
+parser.add_argument("--to",
+                    dest="output",
                     default="list.txt",
                     type=str,
                     help="Path to file with generated results")
@@ -37,7 +37,7 @@ def from_bad_name_to_good(name: str) -> str:
 
     
 def transliterate(name):
-    slovar = {'а':'a','б':'b','в':'v','г':'g','д':'d','е':'e','ё':'e',
+    _dict = {'а':'a','б':'b','в':'v','г':'g','д':'d','е':'e','ё':'e',
           'ж':'zh','з':'z','и':'i','й':'i','к':'k','л':'l','м':'m','н':'n',
           'о':'o','п':'p','р':'r','с':'s','т':'t','у':'u','ф':'f','х':'h',
           'ц':'c','ч':'cz','ш':'sh','щ':'scz','ъ':'','ы':'y','ь':'','э':'e',
@@ -50,21 +50,22 @@ def transliterate(name):
           ':':'',';':'','<':'','>':'','\'':'','"':'','\\':'','/':'','№':'',
           '[':'',']':'','{':'','}':'','ґ':'','ї':'', 'є':'','Ґ':'g','Ї':'i',
           'Є':'e', '—':''}
-    for key in slovar:
-        name = name.replace(key, slovar[key])
+    for key in _dict:
+        name = name.replace(key, _dict[key])
     return name
 
 
 def main():
     result = []
-    # If true - Terminal, false - pipe mode
+    # If true - terminal, false - pipe mode
     if stdin.isatty():
         with open(args.input, "r", encoding="utf-8") as source_file:
             for n, line in enumerate(source_file):
                 try:
-                    new_line = from_bad_name_to_good(line)
                     if args.transliteration:
-                        new_line = transliterate(new_line).lower() + "\n"
+                        new_line = from_bad_name_to_good(transliterate(line).lower()) + "\n"
+                    else:
+                        new_line = from_bad_name_to_good(line) + "\n"
                     result.append(new_line)
                 except IndexError:
                     print(f"Something wrong with {n+1} line ({repr(line)})")
@@ -75,14 +76,15 @@ def main():
     else:
         for n, line in enumerate(stdin):
             try:
-                new_line = from_bad_name_to_good(line)
                 if args.transliteration:
-                    new_line = transliterate(new_line).lower()
+                    new_line = from_bad_name_to_good(transliterate(line).lower())
+                else:
+                    new_line = from_bad_name_to_good(line)
                 result.append(new_line)
             except IndexError:
                 print(f"Something wrong with {n+1} line ({repr(line)})")
         if not args.off_generator:
-            with open(args.output, "w") as out_file:
+            with open(args.output, "w", encoding="utf-8") as out_file:
                 for line in result:
                     out_file.write(line)
         else:
